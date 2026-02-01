1971086e983c3fa2b2c5ff0213aef6b34ceb7851
c-paloma <carmelo.torres@informatica.palomafp.org>
2026-02-01 18:48:16 +0100

    chore: migrate to vercel zero config (api/index.py)

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import sys
+
+app = FastAPI()
+
+@app.get("/api/sanity")
+def sanity():
+    return {"status": "alive", "version": "v3-ZERO-CONFIG", "python": sys.version}
+
+@app.get("/api/{full_path:path}")
+def catch_all(full_path: str):
+    return {"message": "Caught by Zero Config", "path": full_path}
